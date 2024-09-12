16151ebca3431dda138a78842917a50e8a5b0050
ankitsinha <ankitsinha@localhost>
2024-09-12 13:03:21 +0530

    This is my first commit

---
+++ src/com/git/project/Test1.java
@@ -0,0 +1,16 @@
+package com.git.project;
+
+class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		if("YES".equalsIgnoreCase(hungry)){
+			System.out.println(" I am hungry");
+		}else
+		{
+			System.out.println("I am not hungry");
+		}
+	}
+
+}
