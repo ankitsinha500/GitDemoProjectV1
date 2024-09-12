fb8b0ed2e366aac916ba3139c686e9e15f1e5834
ankitsinha <ankitsinha@localhost>
2024-09-12 18:59:00 +0530

    Reviewed and commitred

--- src/com/git/project/Test2.java
+++ src/com/git/project/Test2.java
@@ -1,10 +1,15 @@
 package com.git.project;
 
-import java.security.DomainCombiner;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		String str= "Yes";
+		if("Yes".equalsIgnoreCase(str)) {
+			System.out.println("Hello");
+		}else
+		System.out.println("Bye");
+		
 	}
 }
